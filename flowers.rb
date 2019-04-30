fb65fbce52e3e6b08bbad603e167887799502105
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2019-04-30 18:42:38 -0500

    Initial commit

---
+++ flowers.rb
@@ -0,0 +1,14 @@
+# @param {Integer[]} flowerbed
+# @param {Integer} n
+# @return {Boolean}
+def can_place_flowers(flowerbed, n)
+
+end
+
+can_place_flowers([1,0,0,0,1], 1)
+
+# output: true
+
+can_place_flowers([1,0,0,0,1], 2)
+
+# output: false
